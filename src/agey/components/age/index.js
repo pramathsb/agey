5f0c2631f49a8b7c9d0d5763a8a242a22a943776
Pramath S.B <pramathsb@gmail.com>
2021-08-03 20:09:49 +0530

    Dynamic fields

--- src/agey/components/age/index.js
+++ src/agey/components/age/index.js
@@ -7,6 +7,20 @@ let sid = [];
 export default function Age() {
   const [time, setTime] = useState(null);
   const [init, setInit] = useState(false);
+  const [dob, setDob] = useState("1989-11-17");
+  const [job, setJob] = useState("2014-05-14");
+  const [dobTime, setDobTime] = useState("08:50");
+
+  const changeDob = (e) => {
+    setDob(e.target.value);
+  };
+
+  const changeDobTime = (e) => {
+    setDobTime(e.target.value);
+  };
+  const changeJob = (e) => {
+    setJob(e.target.value);
+  };
 
   useEffect(() => {
     setInit(true);
@@ -14,25 +28,17 @@ export default function Age() {
     sid.length = 0;
     sid.push(
       setTimeout(() => {
-        setTime(new Date());
+        setTime(moment());
       }, 1000)
     );
   }, [time]);
 
   const currentMoment = moment(),
     currentMoment1 = moment(),
-    birthDayMoment = moment("17/11/1989 08:50AM", "DD/MM/YYYY HH:mm"),
-    jobFirstDayMoment = moment("14/05/2014", "DD/MM/YYYY"),
+    birthDayMoment = moment(dob + dobTime, "YYYY-MM-DD HH:mm"),
+    jobFirstDayMoment = moment(job, "YYYY-MM-DD"),
     nextBirthDayMoment = moment(birthDayMoment).year(currentMoment.year()),
     prevBirthDayMoment = moment(nextBirthDayMoment).subtract(1, "year"),
-    livedFor = {
-      years: currentMoment.diff(birthDayMoment, "years"),
-      months: currentMoment.diff(birthDayMoment, "months"),
-      days: currentMoment.diff(birthDayMoment, "days"),
-      hours: currentMoment.diff(birthDayMoment, "hours"),
-      minutes: currentMoment.diff(birthDayMoment, "minutes"),
-      seconds: currentMoment.diff(birthDayMoment, "seconds"),
-    },
     actualAge = {
       months: currentMoment.diff(prevBirthDayMoment, "months"),
       days() {
@@ -59,6 +65,24 @@ export default function Age() {
           .subtract(this.minutes(), "minutes")
           .diff(prevBirthDayMoment, "seconds");
       },
+      milliSeconds() {
+        return currentMoment
+          .subtract(this.months, "months")
+          .subtract(this.days() - 1, "days")
+          .subtract(this.hours(), "hours")
+          .subtract(this.minutes(), "minutes")
+          .subtract(this.seconds(), "seconds")
+          .diff(prevBirthDayMoment, "milliseconds");
+      },
+    },
+    livedFor = {
+      years: currentMoment.diff(birthDayMoment, "years"),
+      months: currentMoment.diff(birthDayMoment, "months"),
+      days: currentMoment.diff(birthDayMoment, "days"),
+      hours: currentMoment.diff(birthDayMoment, "hours"),
+      minutes: currentMoment.diff(birthDayMoment, "minutes"),
+      seconds: currentMoment.diff(birthDayMoment, "seconds"),
+      milliseconds: currentMoment.diff(birthDayMoment, "milliseconds"),
     },
     jobExperience = {
       years: currentMoment1.diff(jobFirstDayMoment, "years"),
@@ -95,34 +119,61 @@ export default function Age() {
     <div>
       <>
         <h1 className="my-4">Whats your Age?</h1>
-        <hr />
         <div className="row">
           <div className="col">
-            <h4 className="my-4">Actual Age:</h4>
-            {renderText(livedFor.years, "Years")}
-            {renderText(actualAge.months % 12, "Months")}
-            {renderText(actualAge.days(), "Days")}
-            {renderText(actualAge.hours(), "Hours")}
-            {renderText(actualAge.minutes(), "Minutes")}
-            {renderText(actualAge.seconds(), "Seconds")}
-            <div className="ms"></div>
+            <div class="input-group">
+              <span class="input-group-text">D.O.B</span>
+              <input class="form-control" type="date" name="" id="" value={dob} max={`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`} onChange={changeDob} />
+            </div>
           </div>
           <div className="col">
-            <h4 className="my-4">You have lived for:</h4>
-            {renderText(livedFor.years, "Years")}
-            {renderText(livedFor.months, "Months")}
-            {renderText(livedFor.days, "Days")}
-            {renderText(livedFor.hours, "Hours")}
-            {renderText(livedFor.minutes, "Minutes")}
-            {renderText(livedFor.seconds, "Seconds")}
+            <div class="input-group">
+              <span class="input-group-text">T.O.B</span>
+              <input class="form-control" type="time" name="" id="" value={dobTime} onChange={changeDobTime} />
+            </div>
           </div>
           <div className="col">
-            <h4 className="my-4">Job Experience:</h4>
-            {renderText(jobExperience.years, "Years")}
-            {renderText(jobExperience.months(), "Months")}
-            {renderText(jobExperience.days(), "Days")}
+            <div className="col">
+              <div class="input-group">
+                <span class="input-group-text">D.O.J</span>
+                <input class="form-control" type="date" name="" id="" value={job} max={`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`} onChange={changeJob} />
+              </div>
+            </div>
           </div>
         </div>
+
+        <hr />
+        {livedFor.minutes > 0 && (
+          <div className="row">
+            <div className="col">
+              <h4 className="my-4">Actual Age:</h4>
+              {renderText(livedFor.years, "Years")}
+              {renderText(actualAge.months % 12, "Months")}
+              {renderText(actualAge.days(), "Days")}
+              {renderText(actualAge.hours(), "Hours")}
+              {renderText(actualAge.minutes(), "Minutes")}
+              {renderText(actualAge.seconds(), "Seconds")}
+              {/* {renderText(actualAge.milliSeconds(), "MilliSeconds")} */}
+              <div className="ms"></div>
+            </div>
+            <div className="col-5">
+              <h4 className="my-4">You have lived for:</h4>
+              {renderText(livedFor.years, "Years")}
+              {renderText(livedFor.months, "Months")}
+              {renderText(livedFor.days, "Days")}
+              {renderText(livedFor.hours, "Hours")}
+              {renderText(livedFor.minutes, "Minutes")}
+              {renderText(livedFor.seconds, "Seconds")}
+              {/* {renderText(livedFor.milliseconds, "MilliSeconds")} */}
+            </div>
+            <div className="col">
+              <h4 className="my-4">Job Experience:</h4>
+              {renderText(jobExperience.years, "Years")}
+              {renderText(jobExperience.months(), "Months")}
+              {renderText(jobExperience.days(), "Days")}
+            </div>
+          </div>
+        )}
       </>
     </div>
   );
